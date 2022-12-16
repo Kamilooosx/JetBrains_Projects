3acf293777ce4b27043cbfcc22225e2f08840cdc
Kamil Ficek <kficek73@gmail.com>
2022-12-16 01:39:10 +0100

    Stage 3/6 In this stage, you need to improve the previous program. Now you will check amounts of water, milk, and coffee beans available in your coffee machine at the moment.

--- Coffee Machine - easy/main.js
+++ Coffee Machine - easy/main.js
@@ -5,14 +5,40 @@
 // You will need this in the following stages
 const input = require('sync-input')
 
-console.log(`Write how many cups of coffee you will need:`);
-
-let howManyCups = input();
 let waterForOneCoffee = 200;
 let milkForOneCoffee = 50;
 let beansForOneCoffee = 15;
 
-console.log(`For ${howManyCups} cups of coffee you will need:
-${waterForOneCoffee * howManyCups} ml of water
-${milkForOneCoffee * howManyCups} ml of milk
-${beansForOneCoffee * howManyCups} g of coffee beans`);
+console.log(`Write how many ml of water the coffee machine has:`);
+let machineWater = input();
+
+console.log(`Write how many ml of milk the coffee machine has:`);
+let machineMilk = input();
+
+console.log(`Write how many grams of coffee beans the coffee machine has:`);
+let machineBeans = input();
+
+console.log(`Write how many cups of coffee you will need:`);
+let howManyCupsYouNeed = input();
+let cups = 0;
+
+while (machineWater >= waterForOneCoffee &&
+machineMilk >= milkForOneCoffee &&
+machineBeans >= beansForOneCoffee){
+    machineWater -= waterForOneCoffee;
+    machineMilk -= milkForOneCoffee;
+    machineBeans -= beansForOneCoffee;
+    ++cups;
+}
+
+switch (true) {
+    case cups == howManyCupsYouNeed:
+        console.log("Yes, I can make that amount of coffee");
+        break;
+    case cups > howManyCupsYouNeed:
+        console.log(`Yes, I can make that amount of coffee (and even ${cups - howManyCupsYouNeed} more than that)`);
+        break;
+    case cups < howManyCupsYouNeed:
+        console.log(`No, I can make only ${cups} cups of coffee`);
+        break;
+}
